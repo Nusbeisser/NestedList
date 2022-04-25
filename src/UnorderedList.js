84f35345a054524aafd38830fd7dabf86b44372c
Nusbeisser <nusbeisser@gmail.com>
2022-04-25 20:38:17 +0200

    scss rename

--- src/UnorderedList.js
+++ src/UnorderedList.js
@@ -1,5 +1,5 @@
 import ListItem from "./ListItem";
-import "./NestedList.scss";
+import "./UnorderedList.scss";
 
 function UnorderedList({ data, section, type, name }) {
   // foo for handle closing checked radio
